f22a3060316b5814d19331842c253c07bf3735d0
Emil Gafarov <113677882+gafarovExe@users.noreply.github.com>
2022-11-07 22:50:39 +0300

    Update Task2.cs

--- Task2.cs
+++ Task2.cs
@@ -18,25 +18,25 @@ class HelloWorld
     {
         string text;
         string a;
-        int k=-1, v=-1;
+        int k = -1, v = -1;
         text = Console.ReadLine();
         char[] chars = new char[text.Length + 2];
         chars[0] = '1';
         chars[chars.Length - 1] = '1';
-        for (int i=0; i<text.Length; i++)
+        for (int i = 0; i < text.Length; i++)
         {
-            chars[i+1] = text[i];
+            chars[i + 1] = text[i];
         }
-        for (int i=0; i<chars.Length-1; i++)
+        for (int i = 0; i < chars.Length - 1; i++)
         {
             bool q = bukavabool(chars[i]);
             bool p = bukavabool(chars[i + 1]);
-            if (q == false && p == true){k=i+1; }
-            if (q == true && p == false) { v = i ; }
-            if (k>-1 && v > -1)
+            if (q == false && p == true) { k = i + 1; }
+            if (q == true && p == false) { v = i; }
+            if (k > -1 && v > -1)
             {
                 List<char> g = new List<char>();
-                for (int j=k; j<=v; j++)
+                for (int j = k; j <= v; j++)
                 {
                     g.Add(chars[j]);
                 }
@@ -51,7 +51,36 @@ class HelloWorld
                 v = -1;
             }
         }
-        for (int i=1; i<chars.Length-1; i++)
+        for (int i = 1; i < chars.Length - 1; i++)
+        {
+            Console.Write(chars[i]);
+        }
+        Console.WriteLine();
+        for (int i = 0; i < chars.Length - 1; i++)
+        {
+            bool q = bukavabool(chars[i]);
+            bool p = bukavabool(chars[i + 1]);
+            if (q == false && p == true) { k = i + 1; }
+            if (q == true && p == false) { v = i; }
+            if (k > -1 && v > -1)
+            {
+                List<char> g = new List<char>();
+                for (int j = k; j <= v; j++)
+                {
+                    g.Add(chars[j]);
+                }
+                g.Reverse();
+                int w = 0;
+                for (int j = k; j <= v; j++)
+                {
+                    chars[j] = g[w];
+                    w++;
+                }
+                k = -1;
+                v = -1;
+            }
+        }
+        for (int i = 1; i < chars.Length - 1; i++)
         {
             Console.Write(chars[i]);
         }
